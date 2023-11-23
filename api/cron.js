62f8b16f6da22c76d21d1e53cc67aae8ac3bba02
Onarbf <onarbf@gmail.com>
2023-11-23 15:36:57 +0100

    --

--- api/cron.js
+++ api/cron.js
@@ -1,7 +1,6 @@
 import { postTweet } from "../bot";
 
 export default async function handler(req, res) {
-    const response = await postTweet('Días que quedan para la legislatura de Milei :');
-    console.log(response);
+    const response = await postTweet('');
     res.status(200).send('Tweeted');
   }
